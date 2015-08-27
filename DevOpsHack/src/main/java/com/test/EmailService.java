13002c5c47a299405b7296fcb2fbd4217d3f9631
RaviParamasivan <ravi.paramasivam.m@gmail.com>
2015-08-27 16:32:12 -0700

    Update EmailService.java

--- DevOpsHack/src/main/java/com/test/EmailService.java
+++ DevOpsHack/src/main/java/com/test/EmailService.java
@@ -2,81 +2,54 @@ package com.test;
 
 import java.util.Properties;
 
-import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+
+
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.SendFailedException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.junit.Test;
 
 public class EmailService {
 
 
-public void test(){
-    Properties props = System.getProperties();
-    props.put("mail.smtp.starttls.enable", true); // added this line
-    props.put("mail.smtp.host", "smtp.gmail.com");
-    props.put("mail.smtp.user", "username");
-    props.put("mail.smtp.password", "password");
-    props.put("mail.smtp.port", "587");
-    props.put("mail.smtp.auth", true);
+public void test(){String to = "gdmmadhan@yahoo.in";//change accordingly  
+		String from = "ravi.paramasivam@gmail.com";//change accordingly  
+		String host = "my-java-app.jigarthanda.cfapps.io";//or IP address  
 
+		//Get the session object  
+		Properties properties = System.getProperties();  
+		properties.setProperty("mail.smtp.host", host);  
+		Session session = Session.getDefaultInstance(properties);  
 
+		//compose the message  
+		try{  
+		MimeMessage message = new MimeMessage(session);  
+		message.setFrom(new InternetAddress(from));  
+		message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+		message.setSubject("Ping");  
+		message.setText("Hello, this is example of sending email  ");  
 
-    Session session = Session.getInstance(props,null);
-    MimeMessage message = new MimeMessage(session);
+		// Send message  
+		Transport.send(message);  
+		System.out.println("message sent successfully....");  
 
-    System.out.println("Port: "+session.getProperty("mail.smtp.port"));
-
-    // Create the email addresses involved
-    try {
-        InternetAddress from = new InternetAddress("username");
-        message.setSubject("Yes we can");
-        message.setFrom(from);
-        message.addRecipients(Message.RecipientType.TO, InternetAddress.parse("receivermail"));
-
-        // Create a multi-part to combine the parts
-        Multipart multipart = new MimeMultipart("alternative");
-
-        // Create your text message part
-        BodyPart messageBodyPart = new MimeBodyPart();
-        messageBodyPart.setText("some text to send");
-
-        // Add the text part to the multipart
-        multipart.addBodyPart(messageBodyPart);
-
-        // Create the html part
-        messageBodyPart = new MimeBodyPart();
-        String htmlMessage = "Our html text";
-        messageBodyPart.setContent(htmlMessage, "text/html");
-
-
-        // Add html part to multi part
-        multipart.addBodyPart(messageBodyPart);
-
-        // Associate multi-part with message
-        message.setContent(multipart);
-
-        // Send message
-        Transport transport = session.getTransport("smtp");
-        transport.connect("smtp.gmail.com", "rsnvinesh@gmail.com", "V$harmila2813");
-        System.out.println("Transport: "+transport.toString());
-        transport.sendMessage(message, message.getAllRecipients());
-
-
-    } catch (AddressException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    } catch (MessagingException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-}
+		}catch (MessagingException mex) {mex.printStackTrace();}  
+		
+		}
+    
 }
