f0b23b2eccc77d6c08af4772144d33ec7b283b03
RaviParamasivan <ravi.paramasivam.m@gmail.com>
2015-08-28 14:23:06 -0700

    Update EmailService1.java

--- DevOpsHack/src/main/java/com/test/EmailService1.java
+++ DevOpsHack/src/main/java/com/test/EmailService1.java
@@ -1,63 +1,61 @@
 package com.test;
-
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 public class EmailService1 {
-    
-    public static String sendEmail(String toEmail, String subject, String body) {
 
-        /*final String userName = "info@catchmycity.com";
-        final String passWord = "Strong123";
-        String fromEmail = "info@catchmycity.com";
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "vps.catchmycity.com");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-        props.put("mail.debug", "false");*/
-        
-        final String userName = "madhan@itechgenie.com";
-        final String passWord = "Verizon@123";
-        String fromEmail = "madhan@itechgenie.com";
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "ns11-999.999servers.com");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-        props.put("mail.debug", "true");
-        Session session;
-        session = Session.getDefaultInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(userName, passWord);
-            }
-        });
+    private static final String SMTP_HOST_NAME = "smtp.sendgrid.net";
+    private static final String SMTP_AUTH_USER = "ImwXeeXWQ4nH7881";
+    private static final String SMTP_AUTH_PWD  = "nmk1EfU0iQ";
 
-        session.setDebug(false);
-        MimeMessage message = new MimeMessage(session);
-        try {
-            message.setFrom(new InternetAddress(fromEmail));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
-            message.setSubject(subject);
-            message.setText(body);
-            Transport.send(message);
-            return "success";
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            return "failure";
-        }
+    public static void main(String[] args) throws Exception{
+       new EmailService1().test();
     }
 
     public void test() throws Exception{
-    	String mailResult = sendEmail("gdmmadhan@yahoo.in", "subject", " body content");    	
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.host", SMTP_HOST_NAME);
+        props.put("mail.smtp.port", 587);
+        props.put("mail.smtp.auth", "true");
+
+        Authenticator auth = new SMTPAuthenticator();
+        Session mailSession = Session.getDefaultInstance(props, auth);
+        // uncomment for debugging infos to stdout
+        // mailSession.setDebug(true);
+        Transport transport = mailSession.getTransport();
+
+        MimeMessage message = new MimeMessage(mailSession);
+
+        Multipart multipart = new MimeMultipart("alternative");
+
+        BodyPart part1 = new MimeBodyPart();
+        part1.setText("This is multipart mail and u read part1……");
+
+        BodyPart part2 = new MimeBodyPart();
+        part2.setContent("<b>This is multipart mail and u read part2……</b>", "text/html");
+
+        multipart.addBodyPart(part1);
+        multipart.addBodyPart(part2);
+
+        message.setContent(multipart);
+        message.setFrom(new InternetAddress("rsnvinesh@gmail.com"));
+        message.setSubject("This is the subject");
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress("gdmmadhan@yahoo.in"));
+
+        transport.connect();
+        transport.sendMessage(message,message.getRecipients(Message.RecipientType.TO));
+        transport.close();
+    }
+
+    private class SMTPAuthenticator extends javax.mail.Authenticator {
+        public PasswordAuthentication getPasswordAuthentication() {
+           String username = SMTP_AUTH_USER;
+           String password = SMTP_AUTH_PWD;
+           return new PasswordAuthentication(username, password);
+        }
     }
 }
