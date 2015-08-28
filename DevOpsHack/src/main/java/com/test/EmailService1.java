e1702dbdfbf4ad5a32c28a99c750580566150070
RaviParamasivan <ravi.paramasivam.m@gmail.com>
2015-08-27 17:48:00 -0700

    Update EmailService1.java

--- DevOpsHack/src/main/java/com/test/EmailService1.java
+++ DevOpsHack/src/main/java/com/test/EmailService1.java
@@ -7,17 +7,20 @@ import java.util.Properties;
 public class EmailService1 {
     private static final String SMTP_HOST_NAME = "smtp.gmail.com";
     private static final int SMTP_HOST_PORT = 587;//465,587,25
-    private static final String SMTP_AUTH_USER = "dummyrls@yahoo.com";
-    private static final String SMTP_AUTH_PWD  = "my password";
+    private static final String SMTP_AUTH_USER = "rsnvinesh@gmail.com";
+    private static final String SMTP_AUTH_PWD  = "V$harmila2813";
 
  
 
     public void test() throws Exception{
+    	
+    	
         Properties props = new Properties();
 
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.host", SMTP_HOST_NAME);
         props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
         // props.put("mail.smtps.quitwait", "false");
 
         Session mailSession = Session.getDefaultInstance(props);
