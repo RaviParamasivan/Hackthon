a07bfa47078e224cb5a55af47d3f9389c9a2bbb7
RaviParamasivan <ravi.paramasivam.m@gmail.com>
2015-08-28 01:13:24 -0700

    Update MyServlet.java

--- DevOpsHack/src/main/java/com/test/hackathon/controler/MyServlet.java
+++ DevOpsHack/src/main/java/com/test/hackathon/controler/MyServlet.java
@@ -19,27 +19,25 @@ public class MyServlet extends HttpServlet {
     	PrintWriter pw = theResponse.getWriter();
     	theResponse.setContentType("text/html");
     	try{
-            JSONObject json = new JSONObject(theRequest.getParameter("jsonStr"));
-            Healper helper = new Healper();
-            boolean isValid= helper.isValid(json);
-            EmailService1 email= new EmailService1();
-            email.test();
-            Jira jiraInterface = new Jira();
-            boolean isUpdated =jiraInterface.updateJira(json);
-            System.out.println("json "+json);
-            pw.print("User Feedback updated successfully");
-            if(isUpdated){
-            	pw.print("User Feedback updated successfully");
-            }
-           
+    		if(theRequest.getParameter("jsonStr")!= null){
+        		System.out.println("theRequest"+theRequest );
+                JSONObject json = new JSONObject(theRequest.getParameter("jsonStr"));
+                Healper helper = new Healper();
+                boolean isValid= helper.isValid(json);
+                EmailService1 email= new EmailService1();
+                email.test();
+                Jira jiraInterface = new Jira();
+                boolean isUpdated =jiraInterface.updateJira(json);
+                System.out.println("json "+json);
+                pw.print("User Feedback updated successfully");
+                if(isUpdated){
+                	pw.print("User Feedback updated successfully");
+                }
+    		}
     	}catch (Exception e){
     		pw.print(e);
     		System.out.println("Exception in controler "+e);
     	}
-        
-
-        
-        
 
     }
 }
